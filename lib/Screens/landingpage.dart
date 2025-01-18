660b2c29a49e35b8087337613f9949a6fc19787a
eternity2745 <vsdevan2006@gmail.com>
2025-01-18 16:12:34 +0530

    Add landing page and report incident screen with navigation

--- lib/Screens/landingpage.dart
+++ lib/Screens/landingpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hack_a_addict/Screens/reportIncident.dart';
+import 'package:sizer/sizer.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
@@ -7,13 +9,159 @@ class LandingPage extends StatefulWidget {
   State<LandingPage> createState() => _LandingPageState();
 }
 
-class _LandingPageState extends State<LandingPage> {
+class _LandingPageState extends State<LandingPage> with AutomaticKeepAliveClientMixin {
+
+  
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Screen"),
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(left: 5.w, right: 5.w, top: 5.h),
+        child: Column(
+          children: [
+            Text(
+              "Sample Text with a long quote for drug addiction anti drug addiction",
+              style: TextStyle(
+                fontSize: 0.3.dp,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic
+              ),
+            ),
+            Text(
+              textAlign: TextAlign.end,
+              "- Author Name",
+              style: TextStyle(
+                fontSize: 0.3.dp,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic
+              ),
+            ),
+            SizedBox(height: 5.h),
+            GridView.count(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              crossAxisCount: 2,
+              mainAxisSpacing: 5.w,
+              crossAxisSpacing: 5.w,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => ReportIncidentScreen()));
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blue.shade400,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black87,
+                          spreadRadius: 1,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        )
+                      ]
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Report Incident",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 0.31.dp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold
+                        )
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.blue.shade400,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black87,
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      )
+                    ]
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Rehabilitation Centers",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 0.31.dp,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.blue.shade400,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black87,
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      )
+                    ]
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Quiz",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 0.31.dp,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.blue.shade400,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black87,
+                        spreadRadius: 1,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      )
+                    ]
+                  ),
+                  child: Center(
+                    child: Text(
+                      "AI Assistant",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 0.31.dp,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
+  
+  @override
+
+  bool get wantKeepAlive => true;
 }
---
+++ lib/Screens/reportIncident.dart
@@ -0,0 +1,149 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class ReportIncidentScreen extends StatefulWidget {
+  const ReportIncidentScreen({super.key});
+
+  @override
+  State<ReportIncidentScreen> createState() => _ReportIncidentScreenState();
+}
+
+class _ReportIncidentScreenState extends State<ReportIncidentScreen> {
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Report Incident"),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(left: 5.w, right: 5.w, top: 5.h),
+        child: Column(
+          children: [
+            Text(
+              "Fill in the details of the incident",
+              style: TextStyle(
+                fontSize: 0.3.dp,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic
+              ),
+            ),
+            SizedBox(height: 5.h),
+            TextField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.edit),
+                suffixIconColor: Colors.black54,
+                hintText: "Incident Title",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15)
+                )
+              ),
+            ),
+            SizedBox(height: 3.h),
+            TextField(
+              maxLines: 3,
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.description),
+                suffixIconColor: Colors.black54,
+                hintText: "Incident Description",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15)
+                )
+              ),
+            ),
+            SizedBox(height: 3.h),
+            GestureDetector(
+              onTap: () {
+                log("Tapped");  // Add code to pick location
+              },
+              child: TextField(
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.location_on_outlined),
+                  suffixIconColor: Colors.black54,
+                  hintText: "Pick Incident Location",
+                  enabled: false,
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.black54)
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  )
+                ),
+              ),
+            ),
+            SizedBox(height: 3.h),
+            GestureDetector(
+              onTap: () {
+                log("Tapped");  // Add code to pick location
+              },
+              child: TextField(
+                maxLines: 3,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.image_outlined),
+                  suffixIconColor: Colors.black54,
+                  hintText: "Attach Image (if any)",
+                  enabled: false,
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    borderSide: BorderSide(color: Colors.black54)
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15)
+                  )
+                ),
+              ),
+            ),
+            SizedBox(height: 5.h),
+            ElevatedButton(
+                      onPressed: () {
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(75.w, 6.h),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Colors.blue.shade100,
+                      ),
+                      child: Row(
+                        spacing: 3.w,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Report Incident"),
+                          Icon(Icons.report_outlined)
+                        ],
+                      )
+            ),
+            SizedBox(height: 2.h),
+            ElevatedButton(
+                      onPressed: () {
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: Size(75.w, 6.h),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Colors.blue.shade100,
+                      ),
+                      child: Row(
+                        spacing: 3.w,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Report Incident Anonymously"),
+                          Icon(Icons.private_connectivity_outlined)
+                        ],
+                      )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
